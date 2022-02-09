7cadfce681dcdfff4da3d814759632877bbe16b2
chrismelyan <chris.shelpuk@gmail.com>
2022-02-10 00:56:30 +0300

    Route pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogues from "./components/Dialogues/Dialogues";
+import Dialogues from "./components/Pages/Dialogues/Dialogues";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Music from "./components/Pages/Music/Music";
+import News from "./components/Pages/News/News";
+import Settings from "./components/Pages/Settings/Settings";
 
 function App() {
     return (
@@ -14,8 +17,11 @@ function App() {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Routes>
-                    <Route path={'/dialogues'} element={<Dialogues />} />
-                    <Route path={'/profile'} element={<Profile />}/>
+                        <Route path={'/dialogues'} element={<Dialogues/>}/>
+                        <Route path={'/profile'} element={<Profile/>}/>
+                        <Route path={'/music'} element={<Music/>}/>
+                        <Route path={'/news'} element={<News/>}/>
+                        <Route path={'/settings'} element={<Settings/>}/>
                     </Routes>
                 </div>
             </div>
---
+++ src/components/Pages/Settings/Settings.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div>
+           SETTINGS
+        </div>
+    );
+};
+
+export default Settings;
